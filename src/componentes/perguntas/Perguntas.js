67bc5683e06ee64a3f28240119c4ce633b201dad
João Pedro <pedroraimundodossantos13@gmail.com>
2022-07-25 22:11:46 -0300

    add perguntas2

--- src/componentes/perguntas/Perguntas.js
+++ src/componentes/perguntas/Perguntas.js
@@ -7,12 +7,14 @@ export default function Perguntas() {
         <div className="container">
             <h1>Perguntas frequentes</h1>
             <details class="collapse">
-                <summary class="title">Pergunta 1</summary>
-                <div class="description">Teste</div>
+                <summary class="title">O Wise Up Online é um complemento da escola?</summary>
+                <div class="description">Não. O Wise Up Online é um curso completamente online com conteúdos e metodologia próprias para que você aprenda ou complemente o seu conhecimento na língua inglesa.
+                    <br /><br />Você estuda pela plataforma no seu dispositivo favorito com aulas 100% online e documentários originais onde quiser, e quando puder. Sem horas marcadas ou deslocamento até um endereço.
+                    E com a possibilidade de baixar o material didático de cada módulo do curso.</div>
             </details>
 
             <details class="collapse">
-                <summary class="title">Pergunta 2</summary>
+                <summary class="title">Pergunta 2 </summary>
                 <div class="description">Teste</div>
             </details>
 
